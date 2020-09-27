081571daff72e47aaf0517492c4e39520c27300b
Uzytkownik <uuzytkownik@kursGIT.pl>
2020-09-27 15:18:39 +0200

    Add LifeCycleTest

--- src/test/java/calc/LifeCycleTest.java
+++ src/test/java/calc/LifeCycleTest.java
@@ -1,11 +1,12 @@
 package calc;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import javax.sound.midi.Soundbank;
 
 public class LifeCycleTest {
 
-    private final Calculator calculator = new Calculator();
+ /* private final Calculator calculator = new Calculator();
 
     @Test
     void testSum(){
@@ -19,5 +20,42 @@ public class LifeCycleTest {
     @Test
     void testSum3(){
         Assertions.assertEquals(4, calculator.sum(2,2));
+  */
+
+    @BeforeAll
+    static void beforeAll(){
+        System.out.println("This is called ONCE before all tests");
+
+            }
+
+    @BeforeEach
+    void beforeEach(){
+        System.out.println("This is called before EACH test");
     }
+
+    @Test
+    void test1(){
+        System.out.println("1");
+    }
+
+    @Test
+    void test2(){
+        System.out.println("2");
+    }
+
+    @Test
+    void test3(){
+        System.out.println("3");
+    }
+
+    @AfterEach
+    void afterEach(){
+        System.out.println("This is called after EACH test");
+    }
+
+    @AfterAll
+    static void afterAll(){
+        System.out.println("This is called ONCE after all tests");
+    }
+
 }
